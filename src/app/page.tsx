d11911062275fcbdec261a078356c5dc3423c76f
Prince <princesinha629@gmail.com>
2025-04-10 17:35:47 +0530

    convex database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import {SignedIn,SignedOut, SignInButton, SignUpButton, UserButton,} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>  <h1>hello</h1>
+    <div className="m-20">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
